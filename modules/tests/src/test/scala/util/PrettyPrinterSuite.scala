997b88d3632d7299b50ba1de647a0b40a1b882fe
samgj18 <samgj_18@hotmail.com>
2022-12-15 15:25:36 +0000

    Fixing flaky tests related to PrettyPrinter

--- modules/tests/src/test/scala/util/PrettyPrinterSuite.scala
+++ modules/tests/src/test/scala/util/PrettyPrinterSuite.scala
@@ -8,7 +8,6 @@ import dolphin.util.PrettyPrinter
 
 import weaver.FunSuite
 
-// This test relies on actual line numbers, so it's not a very good test but it's better than nothing.
 object PrettyPrinterSuite extends FunSuite {
   test("PrettyPrinter should return a string") {
     expect(PrettyPrinter.beautify(new Exception("test"), None).isInstanceOf[String])
@@ -23,12 +22,15 @@ object PrettyPrinterSuite extends FunSuite {
     ) and expect(
       PrettyPrinter
         .beautify(new Exception("test"), Some("test"))
-        .contains(dir + "/modules/tests/src/test/scala/util/PrettyPrinterSuite.scala:20")
+        .contains(dir + "/modules/tests/src/test/scala/util/PrettyPrinterSuite.scala")
     ) and expect(
       PrettyPrinter
         .beautify(new Exception("test"), Some("test"))
         .replaceAll("\n", " ")
-        .substring(0, 241) == "  \uD83D\uDD25test\uD83D\uDD25 ❇️An unexpected error occurred: no hint available for this error: java.lang.Exception❇️  Stack trace: test ℹ️ maybe /Users/sjimenez/Desktop/Learning/event-store-db4s/modules/tests/src/test/scala/util/PrettyPrinterSuite.scala:24 ℹ️"
+        .substring(0, 241)
+        .contains(
+          " unexpected error occurred: no hint available for this error: java.lang.Exception❇️  Stack trace: test ℹ️ maybe /Users/sjimenez/Desktop/Learning/event-store-db4s/modules/tests/src/test/scala/util/PrettyPrinterSuite.scala"
+        )
     )
   }
 }
