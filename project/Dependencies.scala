a86ccb09ba1e1178d80c46b9e10fbad985451122
Scala Steward <scala_steward@virtuslab.com>
2023-08-13 18:09:40 +0000

    Update fs2-core to 3.8.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val betterMonadicFor   = "0.3.1"
     val catsCore           = "2.9.0"
     val catsEffect         = "3.5.1"
-    val fs2                = "3.7.0"
+    val fs2                = "3.8.0"
     val eventStoreDbClient = "4.3.0"
     val kindProjector      = "0.13.2"
     val log4cats           = "2.6.0"
