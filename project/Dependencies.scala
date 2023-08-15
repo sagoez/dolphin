88bf9f8df576ef1e0050538f6043eab0a667ce1d
Scala Steward <scala_steward@virtuslab.com>
2023-08-15 19:37:24 +0000

    Update cats-core to 2.10.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   object V {
 
     val betterMonadicFor   = "0.3.1"
-    val catsCore           = "2.9.0"
+    val catsCore           = "2.10.0"
     val catsEffect         = "3.5.1"
     val fs2                = "3.8.0"
     val eventStoreDbClient = "4.3.0"
