1ca57264e199d391f367df0c335fa3f7e645a1d0
Scala Steward <scala_steward@virtuslab.com>
2024-01-16 23:27:17 +0000

    Update cats-effect to 3.5.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
 
     val betterMonadicFor   = "0.3.1"
     val catsCore           = "2.10.0"
-    val catsEffect         = "3.5.2"
+    val catsEffect         = "3.5.3"
     val fs2                = "3.9.3"
     val eventStoreDbClient = "5.2.0"
     val kindProjector      = "0.13.2"
